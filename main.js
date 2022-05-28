20bab90b4ff542f1497ef32eebac8c3c714773bd
Connor Corcoran <connorcorc@gmail.com>
2022-05-28 13:58:00 -0600

    Add hide functionality for delete message button

--- main.js
+++ main.js
@@ -1,31 +1,48 @@
 var radio1 = document.querySelector('#affirmation');
 var radio2 = document.querySelector('#mantra')
 var button = document.querySelector('.receive-message')
+var textOutput = document.querySelector('.output')
+var meditate = document.querySelector('.meditate')
+var box2 = document.querySelector('.white-box2')
+var deleteButton = document.querySelector('.delete-button')
 
 button.addEventListener('click', showMessage)
+deleteButton.addEventListener('click', deleteMessage)
 
-var mantraList = [];
+deleteButton.style.visibility = 'hidden'
 
-function getRandomArray(array) {
-  return Math.floor(Math.random() * array.length);
-}
 
 function showMessage() {
   console.log('this works')
   if (radio1.checked) {
     randomAffirmation()
-  } else if {
+    deleteButton.style.visibility = 'visible'
+  } else if (radio2.checked) {
     randomMantra()
-  }
+    deleteButton.style.visibility = 'visible'
+  } else {
   alert('Click a button!')
+  }
+}
+
+function deleteMessage() {
+  console.log('this works')
+  var affirmation = affirmations;
+  var mantra = mantra;
+
+  showMessage()
+
+  // alert(`We won't show you that message again`)
 }
 
 function randomAffirmation() {
-  var affirmation = Math.floor(Math.random() * array.length)
+  var affirmation = affirmations[Math.floor(Math.random() * affirmations.length)]
+  textOutput.innerText = affirmation;
+  meditate.style.visibility = 'hidden';
 }
 
 function randomMantra() {
-  var mantra = Math.floor(Math.random() * array.length)
+  var mantra = mantras[Math.floor(Math.random() * mantras.length)]
+  textOutput.innerText = mantra;
+  meditate.style.visibility = 'hidden';
 }
-
-function randomMessage(affirmation, mantra)

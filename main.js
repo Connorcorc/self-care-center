2c5c20b84e31e11b8c46e8d76f571387bce2ab81
Connor Corcoran <connorcorc@gmail.com>
2022-05-29 18:58:34 -0600

    Add cleaner code

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ var deleteButton = document.querySelector('.delete-button')
 
 button.addEventListener('click', showMessage)
 deleteButton.addEventListener('click', deleteMessage)
-// deleteButton.style.visibility = 'hidden'
 deleteButton.classList.add('hidden')
 
 function showMessage() {
@@ -18,15 +17,11 @@ function showMessage() {
     deleteButton.classList.remove('hidden')
     textOutput.classList.remove('hidden')
     meditate.classList.add('hidden')
-    // deleteButton.style.visibility = 'visible'
-    // textOutput.style.visibility = 'visible'
   } else if (radio2.checked) {
     randomMantra()
     deleteButton.classList.remove('hidden')
     textOutput.classList.remove('hidden')
     meditate.classList.add('hidden')
-    // deleteButton.style.visibility = 'visible'
-    // textOutput.style.visibility = 'visible'
   } else {
   alert('Click a button!')
   }
@@ -34,15 +29,9 @@ function showMessage() {
 
 function deleteMessage() {
   console.log('this works')
-  for (var i = 0; i < affirmations.length; i++) {
-    console.log(affirmations[i])
-  }
   textOutput.classList.add('hidden')
   meditate.classList.remove('hidden')
   deleteButton.classList.add('hidden')
-
-  // meditate.style.visibility = 'visible';
-  // textOutput.style.visibility = 'hidden';
   alert(`Sorry about that. Select a new message!`)
 }
 
